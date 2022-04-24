a575ed8e5e8b8d78e7a4e07b8cca5459b0babca0
Scecin <silvia.cecin@hotmail.com>
2022-04-24 11:43:50 +0100

    select and select_all functions included in member_repossitory

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,3 +1,4 @@
+from unittest import result
 from db.run_sql import run_sql
 
 from models.member import Member
@@ -9,4 +10,27 @@ def save(member):
     values = [member.first_name, member.last_name]
     results = run_sql(sql, values)
     member.id = results[0]['id']
+    return member
+
+# Select all the members
+def select_all():
+    members = []
+
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+    for row in results:
+        member = Member(row['first_name'], row['last_name'], row['id'])
+        members.append(member)
+    return members
+
+
+# Select just one member
+def select(id):
+    member = None
+    sql = "SELECT * FROM members WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)[0]
+
+    if result is not None:
+        member = Member(result['first_name'], result['last_name'], result['id'])
     return member
