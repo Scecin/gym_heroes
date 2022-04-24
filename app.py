62f1299fa112172c3965e375c362ffde0394c165
Scecin <silvia.cecin@hotmail.com>
2022-04-24 12:56:33 +0100

    In gym_class_controller included index and in templates>gym_class index.hml created

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 from controllers.member_controller import members_blueprint
-# from controllers.gym_class_controller import gym_classes_blueprint
+from controllers.gym_class_controller import gym_classes_blueprint
 # from controllers.gym_controller import gyms_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(members_blueprint)
-# app.register_blueprint(gym_classes_blueprint)
+app.register_blueprint(gym_classes_blueprint)
 # app.register_blueprint(gyms_blueprint)
 
 
--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -1,11 +1,12 @@
-# from flask import Flask, render_template
-# from flask import Blueprint
-# from models.gym_class import Gym_class
-# import repositories.gym_class_repository as gym_class_repository
+from flask import Flask, render_template
+from flask import Blueprint
+from models.gym_class import Gym_class
+import repositories.gym_class_repository as gym_class_repository
 
-# gym_classes_blueprint = Blueprint("gym_classes", __name__)
+gym_classes_blueprint = Blueprint("gym_classes", __name__)
 
-# # INDEX
-# @gym_classes_blueprint.route("/gym_classes")
-# def gym_classes():
-#     gym_classes = gym_class_repository.select_all()
+# INDEX
+@gym_classes_blueprint.route("/gym_classes")
+def gym_classes():
+    gym_classes = gym_class_repository.select_all()
+    return render_template("gym_classes/index.html", gym_classes = gym_classes)
