762f744d8d36dca97e21482557e241fc23ae7864
Scecin <silvia.cecin@hotmail.com>
2022-04-24 10:35:35 +0100

    Classes created and included in models

---
+++ models/class.py
@@ -0,0 +1,7 @@
+class Class:
+
+    def __init__(self, name, teacher, duration, id = None):
+        self.name = name
+        self.teacher = teacher
+        self.duration = duration
+        self.id = id
---
+++ models/gym.py
@@ -0,0 +1,7 @@
+class Gym:
+
+    def __init__(self, name, member, gym_class, id = None ):
+        self.name = name
+        self.member = member
+        self.gym_class = gym_class
+        self.id = id
