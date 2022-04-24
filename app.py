3d2a1c39aca9ba7ebe431517357d4428fd0e400e
Scecin <silvia.cecin@hotmail.com>
2022-04-24 12:06:22 +0100

    Included index in member_controller, and index.html in members in templates

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-from controllers.member_controller import member_blueprint
+from controllers.member_controller import members_blueprint
+# from controllers.gym_class_controller import gym_classes_blueprint
+# from controllers.gym_controller import gyms_blueprint
 
 app = Flask(__name__)
 
-app.register_blueprint(member_blueprint)
+app.register_blueprint(members_blueprint)
+# app.register_blueprint(gym_classes_blueprint)
+# app.register_blueprint(gyms_blueprint)
 
 
 @app.route("/")
@@ -12,4 +16,4 @@ def main():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,6 +1,11 @@
-# from flask import Flask, render_template, request, redirect
-# from flask import Blueprint
-# from models.member import Member
-# import repositories.member_repository as member_repository
+from flask import Flask, render_template
+from flask import Blueprint
+from models.member import Member
+import repositories.member_repository as member_repository
 
-# members_blueprint = Blueprint("members", __name__)
+members_blueprint = Blueprint("members", __name__)
+
+@members_blueprint.route("/members")
+def members():
+    members = member_repository.select_all()
+    return render_template("members/index.html", members = members)
