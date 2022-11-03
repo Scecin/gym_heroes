4613f9fcc5f81851160213b8883e2b4a6c511943
Scecin <silvia.cecin@hotmail.com>
2022-11-03 13:11:05 +0000

    Added .gitignore

--- db/gym.sql
+++ db/gym.sql
@@ -22,6 +22,17 @@ CREATE TABLE gym_classes (
 
 CREATE TABLE bookings (
     id SERIAL PRIMARY KEY,
+    --delete the rows from the child table automatically, when the rows from the parent table are deleted
     member_id INT REFERENCES members(id) ON DELETE CASCADE,
     gym_class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE
 );
+
+-- -- Create some members 
+-- INSERT INTO members (first_name, last_name, membership, state) VALUES ('David', 'Mackenzie', 'Standard', 'Activate');
+-- INSERT INTO members (first_name, last_name, membership, state) VALUES ('Anna', 'Douglas', 'Premium', 'Activate');
+-- INSERT INTO members (first_name, last_name, membership, state) VALUES ('Mary', 'Thomson', 'Standard', 'Activate');
+
+-- -- -- Create some classe 
+-- INSERT INTO gym_classes (name, teacher, duration, capacity, peak_hour, state) VALUES ('BodyCombat', 'Linzi Draw', 45, 25, 'Peak hour', 'Activate');
+-- INSERT INTO gym_classes (name, teacher, duration, capacity, peak_hour, state) VALUES ('Group Cycle', 'Nick Dickson', 45, 20, 'Peak hour', 'Activate');
+-- INSERT INTO gym_classes (name, teacher, duration, capacity, peak_hour, state) VALUES ('Hatha Yoga', 'Judy Elliot', 50, 15, 'off-Peak hours', 'Activate');
--- services/service.py
+++ services/service.py
@@ -1,6 +1,7 @@
 from repositories.member_repository import by_class
 
-#  Check if there's free space in the class
+#  Check if there's free space in the class. Creates a variable = True, so there is usually space in the class, but if there are more members than capacity the variable is false, so I can't make the reservation.
+
 def check_capacity(gym_class):
     capacity = True
 
@@ -8,7 +9,7 @@ def check_capacity(gym_class):
         capacity = False
     return  capacity
 
-# check the membership and hour kind to book a class
+# check the membership and hour kind to book a class. 
 def check_membership_and_peak_hour(gym_class, member):
     membership = False
 
